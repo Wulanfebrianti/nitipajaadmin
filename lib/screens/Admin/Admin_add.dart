862339ebbee8bb469d09ab4d5860675ba8351955
wlnfbny_ <97417513+Wulanfebrianti@users.noreply.github.com>
2023-09-05 10:23:39 +0700

    adminn -wf

--- lib/screens/Admin/Admin_add.dart
+++ lib/screens/Admin/Admin_add.dart
@@ -1,125 +1,230 @@
 import 'package:flutter/material.dart';
-import '../../config/palette.dart';
-import '../Sidebar.dart';
+import 'package:nitipajaadmin/screens/Admin/Admin_add.dart';
 
-import 'package:flutter/material.dart';
 import '../../config/palette.dart';
-import '../Sidebar.dart';
 import 'Admin_Succes.dart';
 
-class AdminAdd extends StatelessWidget {
+
+class AdminAdd extends StatefulWidget {
+  const AdminAdd({super.key});
+
+  @override
+  State<AdminAdd> createState() => _AdminAddState();
+}
+
+class _AdminAddState extends State<AdminAdd> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        actions: [
+          // Search Bar
+          Container(
+            width: 200,
+            margin: EdgeInsets.only(right: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Cari...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 3, horizontal: 8),
+                suffixIcon: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(6)),
+                    color: Palette.activeColor, // Ganti dengan warna yang diinginkan
+                  ),
+                  height: 24,
+                  width: 24,
+                  child: Icon(Icons.search, color: Colors.white),
+                ),
+              ),
+              onChanged: (value) {
+                // Handle onChanged event here
+              },
+            ),
+          ),
+          SizedBox(width: 16),
+          // Dropdown Profil
+          DropdownButton<String>(
+            items: ['Logout']
+                .map((String value) => DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            ))
+                .toList(),
+            onChanged: (String? newValue) {
+              // Handle onChanged event here
+            },
+          ),
+          SizedBox(width: 16),
+        ],
+      ),
       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 150),
-              Text(
-                'Tambah Admin',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.black,
-                ),
+        child: Column(
+          children: [
+            Text(
+              'Tambah Admin',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
               ),
-              SizedBox(height: 35),
-              Container(
-                height: 475,
-                width: 440,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Palette.activeColor),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        hintText: 'Nama',
-                        fillColor: Colors.white,
-                        filled: true,
-                        prefixIcon: Icon(Icons.person, color: Colors.grey,), // Ganti dengan icon yang sesuai
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    TextField(
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Palette.activeColor),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        hintText: 'Email',
-                        fillColor: Colors.white,
-                        filled: true,
-                        prefixIcon: Icon(Icons.email, color: Colors.grey,),
-                      ),
-                    ),
-                    SizedBox(height: 10), // Spasi tambahan antara Email dan Role
-                    TextField(
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Palette.activeColor),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        hintText: 'Role', // Ganti dengan label yang sesuai
-                        fillColor: Colors.white,
-                        filled: true,
-                        prefixIcon: Icon(Icons.note_alt_outlined, color: Colors.grey,), // Ganti dengan icon yang sesuai
-                      ),
-                    ),
-                    SizedBox(height: 50),
-                    Container(
-                      height: 50,
-                      width: 400,
-                      decoration: BoxDecoration(
-                        color: Palette.activeColor,
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      child: TextButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AdminSucces();
-                            },
-                          );
-                        },
-                        child: Center(
-                          child: Text(
-                            'Kirim',
-                            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 500,
+                  height: 400,
+                  color: Colors.white,
+                  padding: EdgeInsets.all(20),
+                  child: UserTable(),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
+class UserTable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 16),
+          Container(
+            height: 475,
+            width: 440,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Nama',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 16), // Jarak antara label dan input
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Palette.activeColor),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      'Email',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 16), // Jarak antara label dan input
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Palette.activeColor),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+
+                // Role
+                Row(
+                  children: [
+                    Text(
+                      'Role',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(width: 18), // Jarak antara label dan input
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Palette.activeColor),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: Palette.activeColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AdminSucces();
+                        },
+                      );
+                    },
+                    child: Center(
+                      child: Text(
+                        'Kirim',
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
